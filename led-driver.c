f75ecdf861bb404a123a7469e3522f712e4712dd
Joao Costa <jmcosta@motorola.com>
2021-10-05 17:34:07 -0300

    Add first version of led-driver
    It contains only dummy definitions for module_init and module_exit.
    
    Signed-off-by: Joao Costa <jmcosta@motorola.com>

---
+++ led-driver.c
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: GPL-2.0
+
+#include <linux/module.h>
+#include <linux/kernel.h>
+
+static int __init led_driver_init(void)
+{
+	pr_info("LED driver: init\n");
+	return 0;
+}
+
+static void __exit led_driver_exit(void)
+{
+	pr_info("LED driver: exit\n");
+}
+
+module_init(led_driver_init);
+module_exit(led_driver_exit);
+
+MODULE_LICENSE("GPL v2");
+MODULE_AUTHOR("Joao Marcos Costa");
+MODULE_DESCRIPTION("LED driver for Raspberry Pi 3 B+");
+MODULE_VERSION("1.0");
