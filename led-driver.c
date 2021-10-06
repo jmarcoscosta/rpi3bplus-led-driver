23647c81424f5d86ccd0584df0feca611f9a73de
Joao Costa <jmcosta@motorola.com>
2021-10-06 17:44:00 -0300

    Remove procfs entry creation
    The procfs entry is being replaced by the LED framework.
    
    Signed-off-by: Joao Costa <jmcosta@motorola.com>

--- led-driver.c
+++ led-driver.c
@@ -4,7 +4,6 @@
 #include <linux/kernel.h>
 #include <linux/leds.h>
 #include <linux/module.h>
-#include <linux/proc_fs.h>
 #include <linux/slab.h>
 #include <linux/uaccess.h>
 
@@ -13,8 +12,6 @@
 #define GPIO_OUTPUT_CLEAR_0 0x28
 #define GPIO_OUTPUT_SET_0 0x1C
 #define IO_MEM_PAGE_SIZE 1024
-
-static struct proc_dir_entry *led_driver_proc_entry;
 
 static char data_buffer[MAX_DATA_BUFFER_LENGTH];
 
@@ -113,11 +110,6 @@ static ssize_t led_driver_write(struct file *file, const char __user *user,
 	return size;
 }
 
-static const struct proc_ops led_driver_proc_fops = {
-	.proc_read = led_driver_read,
-	.proc_write = led_driver_write,
-};
-
 static int __init led_driver_init(void)
 {
 	pr_info("LED driver: init\n");
@@ -132,13 +124,6 @@ static int __init led_driver_init(void)
 
 	driver_data->gpio_registers = ioremap(BCM2837_GPIO_BASE_ADDR, IO_MEM_PAGE_SIZE);
 
-	led_driver_proc_entry = proc_create("led-driver", 0666, NULL,
-					    &led_driver_proc_fops);
-	if (!led_driver_proc_entry) {
-		pr_err("Failed to create procfs entry\n");
-		return -1;
-	}
-
 	return 0;
 }
 
@@ -146,7 +131,6 @@ static void __exit led_driver_exit(void)
 {
 	pr_info("LED driver: exit\n");
 	kfree(driver_data);
-	proc_remove(led_driver_proc_entry);
 	iounmap(driver_data->gpio_registers);
 }
 
